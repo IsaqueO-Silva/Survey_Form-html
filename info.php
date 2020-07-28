21b29cb212ab736044db9f0b460271b39dec0189
Isaque Silva <46305573+IsaqueO-Silva@users.noreply.github.com>
2020-07-28 20:38:51 -0300

    Add files via upload

---
+++ info.php
@@ -0,0 +1,7 @@
+<?php 
+    echo "<h1>Contact info</h1>";
+    echo "<strong>Name:</strong> ", $_POST["name"],
+    "<br><strong>Email:</strong> ", $_POST["email"],
+    "<br><strong>Subject:</strong> ", $_POST["subject"],
+    "<br><strong>Feedback:</strong> ", $_POST["feedback"];
+?>
