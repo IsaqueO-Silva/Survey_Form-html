e04962ad98c3c131a63c5a3b29922012474e9286
Isaque Silva <46305573+IsaqueO-Silva@users.noreply.github.com>
2020-08-11 20:32:01 -0300

    Delete info.php

--- info.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    echo "<h1>Contact info</h1>";
-    echo "<strong>Name:</strong> ", $_POST["name"],
-    "<br><strong>Email:</strong> ", $_POST["email"],
-    "<br><strong>Subject:</strong> ", $_POST["subject"],
-    "<br><strong>Feedback:</strong> ", $_POST["feedback"];
-?>
